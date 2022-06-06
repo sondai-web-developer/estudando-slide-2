cfcfa793d297c3ab0d603980c39bc54f2eaa7a0d
Sondai <leonardo.rodrigues199506@gmail.com>
2022-06-05 22:55:53 -0300

    cálculo de distâncias quando o evento ocorre

--- js/script.js
+++ js/script.js
@@ -2,19 +2,35 @@ function slide(){
     const wrapper = document.querySelector('[data-slide="wrapper"]');
     const lista = document.querySelector('[data-slide="lista"]');
 
+    let distFinalPosition = 0;
+    let distStartX = 0;
+    let distMovement = 0;
+    let distMovePosition = 0;
+
+    function moveSlide(distX){
+        distMovePosition = distX;
+        lista.style.transform = `translate3d(${distX}px, 0, 0)`;
+    }
+
+    function updatePosition(clientX){
+        distMovement = (distStartX - clientX) * 1.6;
+        return distFinalPosition - distMovement;
+    }
+
     function onStart(event){
         event.preventDefault();
+        distStartX = event.clientX;
         wrapper.addEventListener('mousemove', onMove);
-        console.log('mousedown');
     }
 
     function onMove(event){
-        console.log('moveu');
+        const finalPosition = updatePosition(event.clientX);
+        moveSlide(finalPosition);
     }
 
     function onEnd(event){
-        console.log('acabou');
         wrapper.removeEventListener('mousemove', onMove);
+        distFinalPosition = distMovePosition;
     }
 
     function addSlideEvents(){
